76cc672e0147c3fdbb3af60ba5c50764d6129f6b
Rene Collins <107972484+ReneTheMC@users.noreply.github.com>
2022-12-20 15:55:19 -0600

    templates update

--- peeping/urls.py
+++ peeping/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
 ]
 
